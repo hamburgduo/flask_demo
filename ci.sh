395c4fccaaa69e0cebac7e304a920b6c7651fa92
hamburgduo <kanbaoduo@163.com>
2019-05-13 20:35:21 +0800

    update shell scripts support venv.

--- ci.sh
+++ ci.sh
@@ -4,4 +4,6 @@ source /root/venv_employee/bin/activate
 
 ps -ef | grep manage.py | grep -v grep | awk '{print $2}' | xargs kill -9
 
+export BUILD_ID=dontKillMe
 nohup python /var/lib/jenkins/workspace/employee/manage.py > /var/log/flask_web.log 2>&1 &
+sleep 10s
