2fa0cd127016a68be5dfa8278913c68c060bfe23
hamburgduo <kanbaoduo@163.com>
2019-05-13 19:46:45 +0800

    update shell scripts support venv.

--- ci.sh
+++ ci.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd /root
+source venv_employee/bin/activate
 cd /var/lib/jenkins/workspace/employee
 ps -ef | grep "python manage.py" | grep -v grep | awk '{print $2}' | xargs kill -9
 python manage.py
